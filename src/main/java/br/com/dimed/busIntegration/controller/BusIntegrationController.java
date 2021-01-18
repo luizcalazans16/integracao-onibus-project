7e576a055b3e2a733832c8724d0de9d7165f4139
Luiz Calazans <luiz.calazans@viaflow.net>
2021-01-18 13:04:25 -0300

    First commit

---
+++ src/main/java/br/com/dimed/busIntegration/controller/BusIntegrationController.java
@@ -0,0 +1,5 @@
+package br.com.dimed.busIntegration.controller;
+
+public class BusIntegrationController {
+
+}
---
+++ src/test/java/br/com/dimed/integracaoonibus/IntegracaoOnibusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dimed.integracaoonibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoOnibusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
